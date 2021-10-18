eaa8ca435366f6c542a5d51beb5dbcdffd0ad2a8
nitslaszlo <nitslaszlo@gmail.com>
2021-10-18 11:16:38 +0200

    up

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,13 @@
 import { createApp } from "vue";
-import router from "./router";
-import store from "./store";
+import router from "@/router";
+import store from "@/store";
 
-import App from "./App.vue";
-import Axios from "./apis/ajax";
-import mountElementUI from "./utils/elementUI";
+import App from "@/App.vue";
+import mountElementUI from "@/utils/elementUI";
 
 document.title = import.meta.env.VITE_APP_TITLE;
 
 const app = createApp(App);
-
-app.provide("$http", Axios);
 
 app.use(router);
 app.use(store);
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,8 +4,6 @@
     <p class="links">
       <router-link to="/home">Home</router-link>
       <router-link to="/about">About</router-link>
-      <router-link :to="`/dynamic/${id}`">Dynamic</router-link>
-      <router-link :to="`/axios`">Axios</router-link>
       <router-link :to="`/element`">Element UI Plus</router-link>
     </p>
     <router-view v-slot="{ Component }">
@@ -29,7 +27,6 @@ import { useStore } from "vuex";
 const store = useStore();
 
 // eslint-disable-next-line no-bitwise
-const id = computed(() => ~~(Math.random() * 1000));
 const isEven = computed(() => store.getters["m1/isEven"]);
 const add = () => store.commit("m1/increase");
 const asyncAdd = () => store.dispatch("m1/increase");
